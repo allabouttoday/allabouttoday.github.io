d87d2508172e964c86fde36394c5eacf8c592e5e
Shiva-21 <85338561+Shiva-21@users.noreply.github.com>
2021-07-24 15:32:01 +0530

    sad

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,4 +1,4 @@
-<script>
+
     /**
     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
@@ -13,5 +13,3 @@
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
     })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
