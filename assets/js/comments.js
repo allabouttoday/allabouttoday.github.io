3476e74b986c8440a1bdd259037962818ce9c993
Shiva-21 <85338561+Shiva-21@users.noreply.github.com>
2021-07-24 10:13:05 +0530

    commmmmit

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,12 +1,3 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src = "https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js" >< /script>
-
-<!--TODO: Add SDKs
-for Firebase products that you want to use
-https: //firebase.google.com/docs/web/setup#available-libraries -->
-<script src = "https://www.gstatic.com/firebasejs/8.8.0/firebase-database.js"></script>
-
-<script>
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
@@ -19,24 +10,23 @@ https: //firebase.google.com/docs/web/setup#available-libraries -->
     appId: "1:304111767681:web:3170b1cc621a193f4efd80",
     measurementId: "G-76GM885RMT"
   };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-//Rootref is the whole realtime database.
-const rootRef = firebase.database().ref();
-//commentsRef is just the comments data in the database.
-const commentsRef = rootRef.child('comments');
-//list for click on submit.
-documents.getElementById('btnSubmitComment').addEventListener("click", function() {
-  //Replace line breaks in comment with br tags.
-  var newcomment = document.getElementById('txComment').value.replace(/\n/g, "<br>");
-  //Define a new, keyed post.
-  var newPostRef = commentsRef.push();
-  //Fill the new keyed post with data.
-  newPostRef.set({
-    name: document.getElementById('tbName').value.trim(),
-    comment: newcomment.trim(),
-    frompage: location.pathname;
-    when: firebase.database.ServerValue.TIMESTAMP
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //Rootref is the whole realtime database.
+  const rootRef=firebase.database().ref();
+  //commentsRef is just the comments data in the database.
+  const commentsRef= rootRef.child('comments');
+  //list for click on submit.
+  document.getElementById('btnSubmitComment').addEventListener("click",function(){
+    //Replace line breaks in comment with br tags.
+    var newcomment= document.getElementById('txComment').value.replace(/\n/g,"<br>");
+    //Define a new, keyed post.
+    var newPostRef= commentsRef.push();
+    //Fill the new keyed post with data.
+    newPostRef.set({
+      name: document.getElementById('tbName').value.trim(),
+      comment: newcomment.trim(),
+      frompage: location.pathname,
+      when: firebase.database.ServerValue.TIMESTAMP
+    });
   });
-});
-</script>
