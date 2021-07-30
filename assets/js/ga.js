4f360231b220dfb5d4b8efde75578e776f9d24ea
Shiva-21 <85338561+Shiva-21@users.noreply.github.com>
2021-07-30 09:42:46 +0530

    dgrs

--- assets/js/ga.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F0S0FZ1HTH"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-F0S0FZ1HTH');
-</script>
